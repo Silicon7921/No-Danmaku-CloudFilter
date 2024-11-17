05b9aa39a2e58e02f816bad75ebaaf6bd1ddfd2c
CorePomelo <pomeloalt@foxmail.com>
2024-11-17 18:31:04 +0800

    include bangumi

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         No BiliBili Danmaku CloudFilter
 // @namespace    fkcr
-// @version      0.0.2
+// @version      0.0.3
 // @description  干掉弹幕云屏蔽，恢复那些被隐藏的弹幕
 // @author       CorePomelo
 // @license      GPL-3.0
 // @match        https://www.bilibili.com/video/*
+// @match        https://www.bilibili.com/bangumi/play/*
 // @run-at       document-start
 // @icon         https://www.bilibili.com/favicon.ico
 // @grant        none
@@ -25,7 +26,7 @@ if (!unsafeWindow.Map.prototype._set) {
                 danmakuStore.fetchDmSeg = async function () {
                     let result = await danmakuStore._fetchDmSeg(...arguments)
                     for (let danmaku of result.details.elems) {
-                        danmaku.weight = 11
+                        danmaku.weight = 11 // fk chenrui
                     }
                 }
             }
